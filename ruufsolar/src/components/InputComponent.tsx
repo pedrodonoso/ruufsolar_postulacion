84886f6a7cfe1b63cb915ec303c89e422f18f295
pedrodonoso <pedro.donosoa@sansano.usm.cl>
2024-05-30 17:59:37 -0400

    input added

---
+++ ruufsolar/src/components/InputComponent.tsx
@@ -0,0 +1,24 @@
+interface InputComponentArgs {
+  label: string;
+  value: number;
+  setValue: (value: number) => void;
+}
+export default function InputComponent({
+  label,
+  value,
+  setValue,
+}: InputComponentArgs) {
+  return (
+    <div className="p-1 w-1/4">
+      <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        {label}
+      </label>
+      <input
+        value={value}
+        onChange={(e) => setValue(parseInt(e.target.value))}
+        type="text"
+        className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+    </div>
+  );
+}
--- ruufsolar/src/pages/App.tsx
+++ ruufsolar/src/pages/App.tsx
@@ -1,7 +1,30 @@
+import { useState } from "react";
+import InputComponent from "../components/InputComponent";
+
 export default function App() {
+  const [xDimPanel, setxDimPanel] = useState(0);
+  const [yDimPanel, setyDimPanel] = useState(0);
+  const [xDimContainer, setxDimContainer] = useState(0);
+  const [yDimContainer, setyDimContainer] = useState(0);
+
   return (
-    <>
-      <div className="text-2xl">INIT</div>
-    </>
+    <div className="grid justify-items-center p-4 w-screen h-screen bg-zinc-800 text-slate-50 ">
+      <div className="flex flex-col space-y-3 w-3/4">
+        <article className="space-y-3  gap-3">
+          <h1>Cuadrado</h1>
+          <section>
+            <div className="flex flex-row gap-2">
+              <InputComponent value={xDimContainer} setValue={setxDimContainer} label="Dimensión X Container"/>
+              <InputComponent value={yDimContainer} setValue={setyDimContainer} label="Dimensión Y Container"/>
+            </div>
+            <div className="flex flex-row gap-2">
+              <InputComponent value={xDimPanel} setValue={setxDimPanel} label="Dimensión X Panel"/>
+              <InputComponent value={yDimPanel} setValue={setyDimPanel} label="Dimensión Y Panel"/>
+            </div>
+          </section>
+        </article>
+        <div>Triangulo</div>
+      </div>
+    </div>
   );
 }
